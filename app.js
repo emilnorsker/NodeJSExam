33b2636d4a2138ec8be2fda5a7472b09c264e70b
Norsker <norsker@pop-os.localdomain>
2021-05-11 15:49:26 +0200

    added fs and path for ssr

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-const express = require( express );
+const express = require( 'express' );
 const app = express();
+const fs = require( 'fs' );
 
-const PORT = env.PORT ;
+app.use( express.static( 'public' ) );
 
+const header = fs.readFileSync( __dirname + '/public/header/header.html', 'utf-8' );
+const footer = fs.readFileSync( __dirname + '/public/footer/footer.html', 'utf-8' );
+const home = fs.readFileSync( __dirname + '/public/home/home.html', 'utf-8' );
 
+app.get( '/home', ( request, response ) => {
+    response.send( header + home + footer );
+} )
 
+app.get( '*', ( request, response ) => {
+    response.send( 'hello' );
+} )
 
-
-
-app.listen( PORT || 8080, (error) => {
-
-    if( error ) { console.log( error ); }
-
-    console.log( 'server is running on PORT' + PORT );
-
+const server = app.listen( process.env.PORT || 8080, (error) => {
+    if ( error ) { console.log( error ); }
+    console.log( 'Server is running on port', server.address().port );
 });
